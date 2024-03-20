1a5cce233e0736287488af452c9bb008a9b1829c
Niranjan Cholendiran <niranjanajay99@gmail.com>
2024-03-20 05:03:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,8 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write(' ')
 with col2:
-    st.image("resources\CRAB Logo WithBg2.jpg")
+    #st.image("resources\CRAB Logo WithBg2.jpg")
+    st.image("resources/CRAB Logo WithBg2.jpg")
 with col3:
     st.write(' ')
 st.title("CRAB- Colorado Road Assistant Bot")
